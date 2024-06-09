e68dff0332a720671490575f50be07385317d41f
Ibrahim Rayamah <165764319+iBz-04@users.noreply.github.com>
2024-06-09 13:19:28 +0300

    Create prob-4.py

---
+++ prob-4.py
@@ -0,0 +1,17 @@
+##largest palindrme that can be made from two 3-digit nums
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+def largest_palindrome_product():
+    max_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if is_palindrome(product) and product > max_palindrome:
+                max_palindrome = product
+    return max_palindrome
+
+print(largest_palindrome_product())
+
+##answer: 906609
