5d643dc86265fb373bf88ebacddb45b6a7fe9df4
Ibrahim Rayamah <165764319+iBz-04@users.noreply.github.com>
2024-06-12 23:33:46 +0300

    Create Prob-5.py

---
+++ Prob-5.py
@@ -0,0 +1,10 @@
+import functools
+import math
+
+## functools is a module for higher order functions (its python, we just import stuff :)
+##math.gcd(x, y): Calculates the greatest common divisor (GCD) of x and y.
+## x * y // math.gcd(x, y): Computes the least common multiple (LCM) of x and y. 
+
+print (functools.reduce(lambda x,y: x*y//math.gcd(x, y), range(1, 21)))
+
+## output: 232792560
