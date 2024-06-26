836b0af5d35467d973826f290ef9ccedea807078
Ibrahim Rayamah <165764319+iBz-04@users.noreply.github.com>
2024-06-26 20:36:34 +0300

    Create prob-8.cpp

---
+++ prob-8.cpp
@@ -0,0 +1,19 @@
+long long greatest(string num)
+{
+    if (num.length() < 13)
+        return 0;
+
+    long long product = 1;
+    unsigned int i;
+    for (i=0; i<13; ++i) {
+        product *= (num[i]-'0');
+    }
+    long long greatest_product = product;
+   
+    for (i=0; i+13<num.length(); ++i) {
+        product = product/(num[i]-'0')*(num[i+13]-'0');
+        if (greatest_product < product)
+            greatest_product = product;
+    }
+    return greatest_product;
+}
